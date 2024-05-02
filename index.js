42f5971ef1d9b8da93c271edb81c71672862fe3b
Aniket <aniket.d2402@gmail.com>
2024-05-01 21:12:46 -0400

    added routing examples

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //*********************************** FILES ***************************************************/
 //-- Blocking, synchronous way
@@ -29,7 +30,18 @@ const http = require('http');
 // console.log('will read file');
 //*********************************** END ***************************************************/
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview')
+    res.end('This is the overview');
+  else if (pathName === '/product') res.end('This is the product');
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'test-header': 'hello world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
