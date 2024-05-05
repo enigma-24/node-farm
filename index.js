a826819515108ed17eb73b67fc8abfc35cd1698f
Aniket <aniket.d2402@gmail.com>
2024-05-04 22:11:51 -0400

    added code to read json data from file

--- index.js
+++ index.js
@@ -29,13 +29,22 @@ const url = require('url');
 
 // console.log('will read file');
 //*********************************** END ***************************************************/
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === '/' || pathName === '/overview')
     res.end('This is the overview');
   else if (pathName === '/product') res.end('This is the product');
-  else {
+  else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
+  } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
       'test-header': 'hello world',
